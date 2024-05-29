a682ee6e573cccf17f3f5279ea95c6ebbbcbc67d
usha <ushasindhura.thumuluri@ojas-it.com>
2024-05-29 17:15:38 +0530

    added working Angular project Folder

---
+++ Angular_Req2/.history/src/app/index/login/diamond.py
@@ -0,0 +1,8 @@
+i=1
+b=[]
+c=""
+for i in range(10):
+    i=str(i*2)
+    c=c+i
+    print(c.replace(i,"*"))
+    
--- Angular_Req2/src/app/index/offerletter/offerletter.component.ts
+++ Angular_Req2/src/app/index/offerletter/offerletter.component.ts
@@ -6,18 +6,21 @@ import Swal from 'sweetalert2';
 import { HttpClient } from '@angular/common/http';
 import { LocationStrategy } from '@angular/common';
 import { saveAs } from 'file-saver';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
-selector: 'app-offerletter',
-templateUrl: './offerletter.component.html',
-styleUrls: ['./offerletter.component.scss']
+    selector: 'app-offerletter',
+    templateUrl: './offerletter.component.html',
+    styleUrls: ['./offerletter.component.scss']
 })
 export class OfferLetterComponent implements OnInit {
-urlvalues;
-offerLetter: FormGroup;
-data: string;
-message:any;
-constructor( private fb: FormBuilder, private routerNavigate: Router, private ar: ActivatedRoute,
-    private as: ApiservicesService, private http: HttpClient,private location: LocationStrategy) {
+    urlvalues;
+    hrefLink:any;
+    downloadURL: any
+    offerLetter: FormGroup;
+    data: string;
+    message: any;
+    constructor(private fb: FormBuilder, private routerNavigate: Router, private ar: ActivatedRoute,
+        private as: ApiservicesService, private http: HttpClient, private location: LocationStrategy,    private sanitizer: DomSanitizer) {
         this.urlvalues = this.ar.snapshot.queryParams;
         console.log(this.urlvalues);
         history.pushState(null, null, window.location.href);
@@ -26,74 +29,89 @@ constructor( private fb: FormBuilder, private routerNavigate: Router, private ar
             history.pushState(null, null, window.location.href);
             // this.stepper.previous();
         });
-}
-
-ngOnInit() {
-if(this.checkLogin()==false){
-    this.logout();
-}
-this.offerLetter = this.fb.group({
-    name: ['', [  Validators.minLength(6)]],
-    designation: ['', [ Validators.required, Validators.minLength(6)]],
-    ctc: ['', [ Validators.required, Validators.minLength(6)]],
-    description: ['', [ Validators.required, Validators.minLength(6)]],
-    
-});
-}
-checkLogin(){
-    if(localStorage.getItem('token')){
-        return true
-    }else{
-        return false
     }
-}
-logout(){
-    this.routerNavigate.navigate(['/login'])
-    localStorage.removeItem('token')    
-}
-// generate(){
-//     let payload:any={
-//         name:this.offerLetter.value.name,
-//         designation:this.offerLetter.value.designation,
-//         ctc:this.offerLetter.value.ctc,
-//         description:this.offerLetter.value.description
-//     }
-//     this.as.generateOffer(payload).subscribe((res:any)=>{
-//         console.log("res",res)
-//         this.message=res.status
-//         console.log( this.message,"status")
-//         const downloadUrl = res.path; 
-//         this.downloadFile(downloadUrl);
-//     })
-// }
 
-generate() {
-    let payload: any = {
-      name: this.offerLetter.value.name,
-      designation: this.offerLetter.value.designation,
-      ctc: this.offerLetter.value.ctc,
-      description: this.offerLetter.value.description
-    };
-    this.as.generateOffer(payload).subscribe((res: any) => {
-        console.log("res", res);
-        this.message = res.status;
-        console.log(this.message, "status");
-       // Construct file URL
-    const downloadURL = `file:///${res.path.replace(/\\/g, "/")}`;
+    ngOnInit() {
+        if (this.checkLogin() == false) {
+            this.logout();
+        }
+        this.offerLetter = this.fb.group({
+            name: ['', [Validators.minLength(6)]],
+            designation: ['', [Validators.required, Validators.minLength(6)]],
+            ctc: ['', [Validators.required, Validators.minLength(6)]],
+            description: ['', [Validators.required, Validators.minLength(6)]],
 
-    // Trigger download
-    this.downloadFile(downloadURL);
-  });
-}
+        });
+    }
+    checkLogin() {
+        if (localStorage.getItem('token')) {
+            return true
+        } else {
+            return false
+        }
+    }
+    logout() {
+        this.routerNavigate.navigate(['/login'])
+        localStorage.removeItem('token')
+    }
+    // generate(){
+    //     let payload:any={
+    //         name:this.offerLetter.value.name,
+    //         designation:this.offerLetter.value.designation,
+    //         ctc:this.offerLetter.value.ctc,
+    //         description:this.offerLetter.value.description
+    //     }
+    //     this.as.generateOffer(payload).subscribe((res:any)=>{
+    //         console.log("res",res)
+    //         this.message=res.status
+    //         console.log( this.message,"status")
+    //         const downloadUrl = res.path; 
+    //         this.downloadFile(downloadUrl);
+    //     })
+    // }
 
-downloadFile(downloadURL: string) {
-  const link = document.createElement('a');
-  link.setAttribute('href', downloadURL);
-  link.setAttribute('download', 'filename.pdf'); // You can specify the filename here
-  document.body.appendChild(link);
-  link.click();
-  link.remove();
-}
- 
+    generate() {
+        let payload: any = {
+            name: this.offerLetter.value.name,
+            designation: this.offerLetter.value.designation,
+            ctc: this.offerLetter.value.ctc,
+            description: this.offerLetter.value.description
+        };
+        this.as.generateOffer(payload).subscribe((res: any) => {
+            console.log("res", res);
+            this.message = res.status;
+            console.log(this.message, "status");
+            this.downloadURL = res.path;
+            console.log(this.downloadURL, "path")
+            this.downloadURL = this.sanitizer.bypassSecurityTrustUrl(this.downloadURL);
+        //     if (this.downloadURL.startsWith("file:///")) {
+        //     this.downloadURL = this.downloadURL.substring("file:///".length);
+        // }
+            // window.location.href = this.downloadURL;
+            // var link = document.createElement("a");
+            // link.href = this.downloadURL;
+            // console.log(link);
+            // this.hrefLink=link.href.slice(8);
+            // console.log(this.hrefLink,"link")
+            // link.href = this.hrefLink;
+            // console.log(link);
+            // link.download = "sample.pdf"
+            // document.body.appendChild(link);
+            // console.log(this.hrefLink);
+            // link.click();
+            // document.body.removeChild(link);
+            // this.downloadFile(this.downloadURL)
+        });
+
+    }
+    // downloadFile(downloadUrl: string) {
+    //     this.http.get(downloadUrl, { responseType: 'blob' }).subscribe((blob: Blob) => {
+    //         saveAs(blob, 'offer_letter.pdf');
+    //     }, error => {
+    //         console.error("Download error:", error);
+    //     });
+    // }
+
+
 
 }
