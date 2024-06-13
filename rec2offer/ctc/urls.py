0800ecd15dfe356287db1c0e2accf45018c68812
usha <ushasindhura.thumuluri@ojas-it.com>
2024-06-13 16:13:30 +0530

    change dummy table to new table

--- rec2offer/ctc/urls.py
+++ rec2offer/ctc/urls.py
@@ -5,11 +5,13 @@ from . import views
 
 
 urlpatterns = [
-    path('employee-get/', views.HrTeamView.as_view(), name='employee_list'),
-    path('employee-get/<str:id>/', views.HrTeamView.as_view(), name='employee_detail'),
-    path('create-offer-letter/', views.CreateOfferLetter.as_view(), name='employee_detail'),
+    # path('employee-get/', views.HrTeamView.as_view(), name='employee_list'),
+    # path('employee-get/<str:id>/', views.HrTeamView.as_view(), name='employee_detail'),
+    # path('create-offer-letter/', views.CreateOfferLetter.as_view(), name='employee_detail'),
     path('create-column/', views.ColumnCreationAPIView.as_view(), name='create-column'),
     path('add-dummydata/', views.DummyDataAPIView.as_view(), name='add-dummydata'),
+    path('get-dummydata/', views.DummyDataAPIView.as_view(), name='add-dummydata'),
+     
 
     # Uncomment and properly import the following views if they are implemented
     # path('logout/', views.Userlogout.as_view(), name='user_logout'),
