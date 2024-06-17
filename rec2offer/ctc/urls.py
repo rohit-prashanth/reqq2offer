4b529ce433c697c57f9352c687e409d593c709fa
usha <ushasindhura.thumuluri@ojas-it.com>
2024-06-17 10:26:40 +0530

    changes

--- rec2offer/ctc/urls.py
+++ rec2offer/ctc/urls.py
@@ -2,21 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-
-
 urlpatterns = [
+    # API endpoints related to views that are currently active
     # path('employee-get/', views.HrTeamView.as_view(), name='employee_list'),
     # path('employee-get/<str:id>/', views.HrTeamView.as_view(), name='employee_detail'),
     # path('create-offer-letter/', views.CreateOfferLetter.as_view(), name='employee_detail'),
+
     path('create-column/', views.ColumnCreationAPIView.as_view(), name='create-column'),
     path('add-dummydata/', views.DummyDataAPIView.as_view(), name='add-dummydata'),
-    path('get-dummydata/', views.DummyDataAPIView.as_view(), name='add-dummydata'),
-     
-
+    path('get-dummydata/', views.DummyDataAPIView.as_view(), name='get-dummydata'),
+    path('del-dummydata/<field_name>/', views.DeleteFieldAPIView.as_view(), name='del-dummydata'),
+]
     # Uncomment and properly import the following views if they are implemented
+   
+    # path('dropdowndata/', views.CheckboxApiView.as_view(), name='dropdown'),
+
+    # Example of uncommenting and importing views:
     # path('logout/', views.Userlogout.as_view(), name='user_logout'),
     # path('view-user/', views.ViewUser.as_view(), name='view_user'),
     # path('create-user/', views.UserCreate.as_view(), name='create_user'),
     # path('user/<int:id>/', views.GetId.as_view(), name='get_user'),
-]
 
--- rec2offer/ctc/views.py
+++ rec2offer/ctc/views.py
@@ -3,30 +3,28 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from django.contrib.auth import authenticate
 
-from .serializers import (EmployeeViewSerializer, EmployeeDetailsSerializer, 
-                        CustomerSerializer,PDFGenerationSerializer,ColumnCreationSerializer)
-from .models import HrTeam, EmployeeDetails, Customer,NewTable
+from .serializers import (
+    EmployeeViewSerializer, PDFGenerationSerializer, ColumnCreationSerializer
+)
+from .models import HrTeam, EmployeeDetails, Customer, NewTable
 from .create_offer_1 import create_offer
-from rec2offer.settings import BASE_DIR
-import os
 from rec2offer import settings
-from django.db import connection
 
+
+# Imaginary function to handle an uploaded file.
+
+
+import os
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from django.db import connection, models
-from django.core.management import call_command
-from django.apps import apps
-from django.shortcuts import render
-import json
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+
 class HrTeamView(GenericAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -34,122 +32,67 @@ class HrTeamView(GenericAPIView):
     queryset = HrTeam.objects.all()
 
     def get(self, request, id=None):
-        """
-        Handle GET requests for HrTeam.
-        If id is provided, fetch a specific HR team member.
-        If id is not provided, fetch all HR team members.
-        """
         try:
             if id is None:
-                # Fetch all HR team members
                 hr_team_members = self.get_queryset()
                 serializer = self.get_serializer(hr_team_members, many=True)
-                return Response({"fields": serializer.data}, status=status.HTTP_200_OK)
             else:
-                # Fetch a specific HR team member by ID
-                # hr_member = get_object_or_404(self.queryset, pk=id)
-                hr_member = HrTeam.objects.get(pk=id)
+                hr_member = get_object_or_404(self.queryset, pk=id)
                 serializer = self.get_serializer(hr_member)
-                return Response({"fields": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request):
-        """
-        Handle POST requests to create a new HR team member.
-        """
         try:
-            data = request.data
-            serializer = self.get_serializer(data=data)
-
+            serializer = self.get_serializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
-                return Response("created successfully", status=status.HTTP_201_CREATED)
+                return Response("Created successfully", status=status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_403_FORBIDDEN)
-        
+
 
 class CreateOfferLetter(GenericAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = EmployeeViewSerializer
-    queryset = HrTeam.objects.all()
+    serializer_class = PDFGenerationSerializer
 
-    # def post(self, request):
-    #     """
-    #     Handle POST requests to create a new HR team member.
-    #     """
-    #     try:
-    #         data = request.data
-            
-    #         if 'ctc' in data and 'name' in data and 'designation' in data:
-    #             offer = create_offer(data)
-    #             print(offer)
-    #             if offer:
-    #                 return Response(offer, status=status.HTTP_201_CREATED)
-    #             else:
-    #                 return Response({"error":"Salary structure has not created."}, status=status.HTTP_400_BAD_REQUEST)
-    #         else:
-    #             return Response({"error":"Missing required fields"}, status=status.HTTP_400_BAD_REQUEST)
-
-    #     except Exception as e:
-    #         return Response({"error": str(e)}, status=status.HTTP_403_FORBIDDEN)
-    
     def post(self, request, format=None):
-        serializer = PDFGenerationSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            # Get data from serializer
             name = serializer.validated_data['name']
             designation = serializer.validated_data['designation']
+            
             # Generate PDF
-            # pdf_filename = self.generate_pdf(name, designation)
             pdf_filename = create_offer(request.data)
-
+            
             # Save PDF to media directory
             if pdf_filename:
-                # print(pdf_filename.getvalue(), type(pdf_filename))
                 filename = f"offer_letters/{name}_{designation}.pdf"
                 media_path = os.path.join(settings.MEDIA_ROOT, filename)
                 with open(media_path, 'wb') as f:
                     f.write(pdf_filename.getvalue())
-                # Return response
-                return Response({"status":"created successfully","path": os.path.join(settings.MEDIA_URL, filename)}, status=status.HTTP_201_CREATED)
+                return Response({"status": "Created successfully", "path": os.path.join(settings.MEDIA_URL, filename)}, status=status.HTTP_201_CREATED)
             else:
-                return Response({"error":"Salary structure has not created."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "Salary structure has not been created."}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
-    def generate_pdf(self, name, designation):
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename="{name}_{designation}.pdf"'
-        # Create PDF content
-        p = canvas.Canvas(response, pagesize=letter)
-        p.drawString(100, 750, "Name: {}".format(name))
-        p.drawString(100, 730, "Designation: {}".format(designation))
-        # Add more content as needed
-        p.showPage()
-        p.save()
-        return response
-    
 
 class ColumnCreationAPIView(GenericAPIView):
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticated]
-    serializer_class = EmployeeViewSerializer
-    # queryset = HrTeam.objects.all()
-    
+    serializer_class = ColumnCreationSerializer
+
     def post(self, request):
         try:
-            serializer = ColumnCreationSerializer(data=request.data)
+            serializer = self.get_serializer(data=request.data)
             if serializer.is_valid():
                 column_name = serializer.validated_data['column_name']
                 data_type = serializer.validated_data['data_type']
                 table_name = 'ctc_newtable'
 
                 with connection.cursor() as cursor:
-                    # Execute raw SQL to alter table and add column
                     query = f"ALTER TABLE {table_name} ADD COLUMN {column_name} {data_type};"
                     cursor.execute(query)
                 
@@ -157,100 +100,35 @@ class ColumnCreationAPIView(GenericAPIView):
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-
-    # def post(self, request, format=None):
-    #     try:
-    #         serializer = ColumnCreationSerializer(data=request.data)
-    #         if serializer.is_valid():
-    #             column_name = serializer.validated_data['column_name']
-    #             data_type = serializer.validated_data['data_type']
-    #             table_name = serializer.validated_data['table_name']
-    #         # table_name = request.data.get('table_name')
-    #         # column_name = request.data.get('column_name')
-    #         # column_type = request.data.get('column_type')
-
-    #             if not table_name or not column_name or not data_type:
-    #                 return Response({'error': 'Missing parameters'}, status=status.HTTP_400_BAD_REQUEST)
-
-    #             # Validate column type
-    #             column_types = [
-    #                 'char',
-    #                 'text',
-    #                 'integer',
-    #                 'boolean'
-    #             ]
-
-    #             if data_type not in column_types:
-    #                 return Response({'error': 'Invalid column type'}, status=status.HTTP_400_BAD_REQUEST)
-
-    #             myapp = 'ctc'
-                
-    #             # Dynamically add the new field to the model
-    #             model = apps.get_model(myapp, table_name)
-    #             field = models.CharField(max_length=255,null=True) if data_type == 'char' else models.TextField(null=True) if data_type == 'text' else models.IntegerField(null=True) if data_type == 'integer' else models.BooleanField(default=False)
-
-    #             field.contribute_to_class(model, column_name)
-
-    #             # Create a migration for the new field
-    #             call_command('makemigrations', myapp)
-    #             call_command('migrate', myapp)
-
-    #             return Response({'status': 'column added'}, status=status.HTTP_201_CREATED)
-    #         else:
-    #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #     except Exception as e:
-    #         return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-
-
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DummyDataAPIView(GenericAPIView):
-    # authentication_classes = [JWTAuthentication]
-    # permission_classes = [IsAuthenticated]
-    # serializer_class = DummyDataSerializer
-    # queryset = NewTable.objects.all()
-
 
     def get(self, request, format=None):
-        # table_name = request.query_params.get('table_name')
         table_name = 'ctc_newtable'
-        
         
         with connection.cursor() as cursor:
             try:
                 cursor.execute(f'SELECT * FROM {table_name}')
-                # cursor.execute(f"SELECT name FROM sys.columns WHERE object_id = OBJECT_ID({table_name})")
-                columns = [col[0] for col in cursor.description]
-                print(columns)
-                results=columns
-                # rows = cursor.fetchall()
-                # print(rows)
-                # results = [dict(zip(columns, row)) for row in rows]
-                
+                columns = [col[0] for col in cursor.description if col[0] != 'id']  # Exclude 'id' column
+                results = columns
             except Exception as e:
                 return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         
         return Response(results, status=status.HTTP_200_OK)
     
     def post(self, request, format=None):
-        # table_name = request.data.get('table_name')
         table_name = 'ctc_newtable'
         data = request.data
 
-        # if not table_name or not data:
-        #     return Response({'error': 'Table name and data are required'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Validate data format
         if not isinstance(data, dict):
             return Response({'error': 'Data must be a dictionary'}, status=status.HTTP_400_BAD_REQUEST)
 
         columns = ', '.join(data.keys())
         placeholders = ', '.join(['%s'] * len(data))
         values = list(data.values())
-        print(columns)
-        print(placeholders)
-        print(values)
+
         try:
             with connection.cursor() as cursor:
                 query = f'INSERT INTO {table_name} ({columns}) VALUES ({placeholders})'
@@ -258,10 +136,11 @@ class DummyDataAPIView(GenericAPIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({'status': 'record saved'}, status=status.HTTP_201_CREATED)
+        return Response({'status': 'Record saved'}, status=status.HTTP_201_CREATED)
 
 
 class GetDummyDataAPIView(APIView):
+
     def get(self, request, format=None):
         table_name = 'ctc_newtable'
         
@@ -272,7 +151,6 @@ class GetDummyDataAPIView(APIView):
                 rows = cursor.fetchall()
                 results = [dict(zip(columns, row)) for row in rows]
 
-                # Get column details
                 cursor.execute(f"""
                     SELECT column_name, data_type
                     FROM information_schema.columns
@@ -288,24 +166,44 @@ class GetDummyDataAPIView(APIView):
             'columns': column_info,
             'rows': results
         }
-        
-        return Response(response_data, status=status.HTTP_200_OK)
-# class RegistrationAPIView(APIView):
-#     def post(self, request, format=None):
-#         try:
-#             data = request.data
-#             table_name = 'ctc_newtable'
+        return Response(response_data)
+    
+class DeleteFieldAPIView(APIView):
+
+    def delete(self, request, field_name, format=None):
+        table_name = 'ctc_newtable'
+
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(f"""
+                    ALTER TABLE {table_name} DROP COLUMN {field_name};
+                """)
             
-#             # Dynamically generate the insert query
-#             columns = ', '.join(data.keys())
-#             values = ', '.join([f"'{v}'" if isinstance(v, str) else str(v) for v in data.values()])
-#             query = f"INSERT INTO {table_name} ({columns}) VALUES ({values});"
+            return Response({'message': f'Field {field_name} deleted successfully from table {table_name}'}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+# class FileDataAPIView(APIView):
+   
+
+    # def post(self, request, format=None):
+    #     table_name = 'ctc_newtable'
+
+    #     try:
+    #         uploaded_file = request.FILES['upload']
+    #         fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+    #         filename = fs.save(uploaded_file.name, uploaded_file)
+    #         uploaded_file_url = fs.url(filename)
             
-#             with connection.cursor() as cursor:
-#                 cursor.execute(query)
+    #         with connection.cursor() as cursor:
+    #             cursor.execute(f"""
+    #                 INSERT INTO {table_name} (file_url) VALUES (%s)
+    #             """, [uploaded_file_url])
             
-#             return Response({'message': 'Data saved successfully'}, status=status.HTTP_201_CREATED)
-#         except Exception as e:
-#             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(results, status=status.HTTP_200_OK)
+    #         return Response({'message': 'File uploaded successfully'}, status=status.HTTP_201_CREATED)
+
+    #     except Exception as e:
+    #         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
     
